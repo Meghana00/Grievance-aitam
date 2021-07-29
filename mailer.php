b6de73d82a3c7dbd37585ecacc863c97c2732250
Meghana00 <chirravoorumeghana@gmail.com>
2021-07-29 22:27:34 +0530

    updated mailer.php

--- mailer.php
+++ mailer.php
@@ -29,7 +29,7 @@ else{
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<!-- Main CSS -->
-	<link href="./assets/css/main.css" rel="stylesheet" /> </head>
+	<link href="./assets/css/mainstyle.css" rel="stylesheet" /> </head>
 </head>
 
 <body>
@@ -47,47 +47,40 @@ else{
 	<!-- response msg mailer -->
 	<div class="container">
 			<div class="row">
-				<div class="col-sm-9 mt-5">
-					<div class="container mt-3 ">
+				<div class="col-sm-12">
+					<div class="container  ">
 						<!--header row-->
 						<div class="row ">
 							<div class="col d-flex justify-content-center">
-								<div class="col-sm-1 "> <i class="far fa-user-circle ms-3" style="font-size: 3rem;"></i> </div>
+								
 								<div class="col-sm-11 ">
-									<p class="ms-1 fw-lighter text-success" style="font-size:32px;">Grievance Response</p>
+									<p class="ms-1 fw-bold text-success text-success text-center" style="font-size:32px;">Grievance Response</p>
 								</div>
 							</div>
 						</div>
 						<!--end header row-->
 						<div class="row d-flex justify-content-center">
-						
-							<div class="col-sm-12 text-center border mt-5 shadow shadow-regular">
+						    <div class="col-sm-2"></div>
+							<div class="col-sm-8 text-center border mt-5 shadow shadow-regular">
 								<p class="ms-1 fw-lighter text-success p-5" style="font-size:32px;"><?php echo htmlentities($msg) ?></p>
 							</div>
+							<div class="col-sm-2"></div>
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-3 mt-5 p-2">
-					<h3 class="text-success">About Grievance Cell</h3> Our organization has a Grievance Redressal Cell / Faculty Feedback to redress the grievances / Faculty Feedback of its students/Faculty regarding academic matters, library, canteen, hostels, and other central services.</br> Students/Faculty may also post their grievances in the Grievance boxes available in the campus & Feedback facility of students and faculty is also available in AICTE web-portal.</br> &nbsp;
-					<h4 class="text-success">Grievance Cell Committee</h4> 1. Dr D.Vishnu Murthy - Convener</br> 2. Dr D.Azad - Member</br> 3. Sri. Ch.Rajasekhar Rao - Member</br> 4. Sri T.Prabhakara Rao - Member</br> 5. Sri B.Kishore Kumar - Member</br> 6. Dr.M.V.Subba Rao - Member</br>
-					<div class="mb-5"></div>
-				</div>
+				
 			</div>
 		</div>
 		<!-- response msg mailer end -->
+
 	<footer>
-		<div style="background-color:#5cb85c;" class="p-2">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-4"></div>
-					<div class="col-sm-6">
-						<p class="justify-content-md-center text-white">@copyright | WEB SAC-AITAM</p>
-					</div>
-					<div class="col-sm-2"></div>
-				</div>
-			</div>
-		</div>
-	</footer>
+
+   
+<div class="footer-copyright text-center text-light py-1 mt-5" style="background-color:transparent;">
+		<p class="text-success fw-bold">© 2021 Copyright : <a href="https://aitamsac.in/" class="text-danger">aitamsac</a></p>
+</div>
+
+</footer>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
